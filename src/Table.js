5e573ff06f9b0f8c795967dc9028c49fb1a052e6
Gaurav Joshi <gjoshi@calpoly.edu>
2021-01-11 14:53:28 -0800

    Form can submit

--- src/Table.js
+++ src/Table.js
@@ -36,18 +36,5 @@ const TableHeader = () => {
       </table>
     )
   }
-  /*
-  class Table extends Component {
-    render() {
-      const { characterData } = this.props
-  
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={characterData} />
-        </table>
-      )
-    }
-  }*/
 
 export default Table
